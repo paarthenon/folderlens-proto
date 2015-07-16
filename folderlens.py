38c9ee98e602097fdb887766c714c2fcbbd4e7fa
Paarth <paarthk@gmail.com>
2015-07-16 04:35:35 -0400

    initial version functioning

--- folderlens.py
+++ folderlens.py
@@ -1,9 +1,8 @@
 import os
 import sys
 
-Directory = namedtuple('Dictionary',['name','dirs','files'])
 def get_mappings(dest_root, source_root):
-	mapping_list = [] # [(source, destination),...]
+	link_mapping = {} # dest, source mapping
 	for current_path, dirs, files in os.walk(source_root):
 		for file in files:
 			source_path = os.path.join(current_path, file)
@@ -11,22 +10,43 @@ def get_mappings(dest_root, source_root):
 			relative_path = os.path.relpath(source_path, source_root)
 			dest_path = os.path.join(dest_root, relative_path)
 
-			mapping_list.append((source_path, dest_path));
+			link_mapping[dest_path] = source_path
+	return link_mapping
 
-def merge_mappings(list_of_lists):
-	for list in list_of_lists:
-		for source, dest in list:
+def merge_mappings(dict_list):
+	merged = {}
+	duplicates = {}
+	for dict in dict_list:
+		for dest, source in dict.items():
+			if dest in duplicates:
+				duplicates[dest].append(source)
+			else:
+				if dest in merged:
+					duplicates[dest] = [merged.pop(dest), source]
+				else:
+					merged[dest] = source
+	return merged, duplicates
 
-def write_symlinks(list):
-	for source_path, dest_path in list:
+def write_symlinks(dict):
+	for dest_path, source_path in dict.items():
 		#ensure directory for symlink exists
 		os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 		#create symlink
 		os.symlink(source_path, dest_path)
 
 def main(argv):
-	#stop with the indexes
-	makelinks(argv[2],argv[1])
+	out_dir = argv[1]
+	in_dirs = [argv[2], argv[3]]
+
+	#stop with the indexes eventually
+	mappings = [get_mappings(out_dir, path) for path in in_dirs]
+	merged, duplicates = merge_mappings(mappings)
+
+	if len(duplicates) > 0:
+		print("Conflicts found")
+		print(duplicates)
+		
+	write_symlinks(merged)
 
 if __name__ == "__main__":
 	main(sys.argv)
